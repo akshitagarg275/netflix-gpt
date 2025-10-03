08750e3f0cd2c22264d9b24b06a4dfd236bafff4
akshitagarg275 <akshitagarg275@gmail.com>
2025-10-03 11:17:14 +0530

    initial vite and tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div  className="text-5xl text-center font-bold text-green-800 underline">
+        HELLO
+      </div>
+    </>
+  )
+}
+
+export default App
